d32cc17d15a0038c4dca4071764083e62fac3439
Olivia Xiang <oliviafx@gmail.com>
2017-03-22 14:55:49 -0400

    Update testSearch.js

--- testSearch.js
+++ testSearch.js
@@ -1,12 +1,12 @@
 //$("submit").click(function(){
     $.ajax({
-    url: 'https://api.yelp.com/v3/businesses/search',
+    url: 'https://api.yelp.com/v3/businesses/search?location=london',
     type: 'GET',
     beforeSend: function (xhr) {
         xhr.setRequestHeader('Authorization', 'Bearer rUkajLiHIMVFO3mx9jMTIY13XuM8GdVabuF1t37ew4di-l0XD1xhSZ-sKqf7ozxvYEC_WwH-Zn8JSOH73EVA_Jb2oIPDZz_IJ_D6ZE4KAb8nFc9wp6aUb-KDM77SWHYx
 ');
 //    },
-    data: {location:london},
+    data: {},
     success: function (data) { document.getElementById("testContent").innerHTML = data; },
     error: function () {document.getElementById("testContent").innerHTML = "whoops, there was an error"; },
     });
