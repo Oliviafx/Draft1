c4817e9a9f2456244fe16eda924a061f30346f3e
Olivia Xiang <oliviafx@gmail.com>
2017-03-22 01:17:43 -0400

    Update testSearch.js

--- testSearch.js
+++ testSearch.js
@@ -1,1 +1,9 @@
+$("button").click(function(){
+    $.get("demo_test.asp", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+});
 
+$.get(URL,data,function(data,status,xhr),dataType)
+
+// https://www.w3schools.com/jquery/ajax_get.asp
