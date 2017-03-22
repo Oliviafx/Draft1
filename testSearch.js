dbdefe286256091ac26d1ab73a8eaee04c76b85e
Olivia Xiang <oliviafx@gmail.com>
2017-03-22 14:52:47 -0400

    Trying this

--- testSearch.js
+++ testSearch.js
@@ -1,13 +1,13 @@
-$("submit").click(function(){
+//$("submit").click(function(){
     $.ajax({
     url: 'https://api.yelp.com/v3/businesses/search',
     type: 'GET',
     beforeSend: function (xhr) {
         xhr.setRequestHeader('Authorization', 'Bearer rUkajLiHIMVFO3mx9jMTIY13XuM8GdVabuF1t37ew4di-l0XD1xhSZ-sKqf7ozxvYEC_WwH-Zn8JSOH73EVA_Jb2oIPDZz_IJ_D6ZE4KAb8nFc9wp6aUb-KDM77SWHYx
 ');
-    },
+//    },
     data: {location=london},
-    success: function () { },
+    success: function (data) { document.getElementById("testContent").innerHTML = data; },
     error: function () { },
     });
 });
