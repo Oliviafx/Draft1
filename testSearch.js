2c3d61668261da8659fa5be531ae4dfe7fca8c75
Olivia Xiang <oliviafx@gmail.com>
2017-03-22 14:57:23 -0400

    Update testSearch.js

--- testSearch.js
+++ testSearch.js
@@ -3,8 +3,7 @@
     url: 'https://api.yelp.com/v3/businesses/search?location=london',
     type: 'GET',
     beforeSend: function (xhr) {
-        xhr.setRequestHeader('Authorization', 'Bearer rUkajLiHIMVFO3mx9jMTIY13XuM8GdVabuF1t37ew4di-l0XD1xhSZ-sKqf7ozxvYEC_WwH-Zn8JSOH73EVA_Jb2oIPDZz_IJ_D6ZE4KAb8nFc9wp6aUb-KDM77SWHYx
-');
+        xhr.setRequestHeader('Authorization', 'Bearer rUkajLiHIMVFO3mx9jMTIY13XuM8GdVabuF1t37ew4di-l0XD1xhSZ-sKqf7ozxvYEC_WwH-Zn8JSOH73EVA_Jb2oIPDZz_IJ_D6ZE4KAb8nFc9wp6aUb-KDM77SWHYx');
 //    },
     data: {},
     success: function (data) { document.getElementById("testContent").innerHTML = data; },
