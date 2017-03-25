8987595ee8adebad088edd761bbce4f2470271e0
Olivia Xiang <oliviafx@gmail.com>
2017-03-25 02:26:43 -0400

    a starter with node.js

---
+++ back.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+const clientId = '<YOUR_CLIENT_ID>';
+const clientSecret = '<YOUR_CLIENT_SECRET>';
+
+const searchRequest = {
+  term:'Four Barrel Coffee',
+  location: 'san francisco, ca'
+};
+
+yelp.accessToken(clientId, clientSecret).then(response => {
+  const client = yelp.client(response.jsonBody.access_token);
+
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses[0];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  });
+}).catch(e => {
+  console.log(e);
+});
