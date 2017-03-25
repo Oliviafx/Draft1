7a80af924682c002750bb2e943f410317f7786cc
Olivia Xiang <oliviafx@gmail.com>
2017-03-25 14:21:17 -0400

    Update back.js

--- back.js
+++ back.js
@@ -2,8 +2,8 @@
 
 const yelp = require('yelp-fusion');
 
-const clientId = '<YOUR_CLIENT_ID>';
-const clientSecret = '<YOUR_CLIENT_SECRET>';
+const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
+const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
 
 const searchRequest = {
   term:'Four Barrel Coffee',
@@ -12,7 +12,7 @@ const searchRequest = {
 
 yelp.accessToken(clientId, clientSecret).then(response => {
   const client = yelp.client(response.jsonBody.access_token);
-
+  
   client.search(searchRequest).then(response => {
     const firstResult = response.jsonBody.businesses[0];
     const prettyJson = JSON.stringify(firstResult, null, 4);
