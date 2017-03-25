0ecf6d8299daa3e9bfe7bf29bad88aa1884f7d48
Olivia Xiang <oliviafx@gmail.com>
2017-03-25 15:04:05 -0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const YELP = require('node-yelp-fusion');
+const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
+const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
 
 var yelp = new YELP({id:USLc4bO1jd_WNmHyMtyWeQ, secret:CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D});
+
+const token = YELP.accessToken(clientId, clientSecret).then(response => {
+  console.log(response.jsonBody.access_token);
+}).catch(e => {
+  console.log(e);
+});
+
 
 yelp.search("term=Biryani&location=New York")
   .then(function(result){
