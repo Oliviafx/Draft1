48653c55952203699287b1e44cca4e6aaea6860f
Olivia Xiang <oliviafx@gmail.com>
2017-03-25 15:18:37 -0400

    trying this out

--- backend.js
+++ backend.js
@@ -5,8 +5,7 @@ const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
 const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+  location: 'london'
 };
 
 yelp.accessToken(clientId, clientSecret).then(response => {
@@ -20,22 +19,4 @@ yelp.accessToken(clientId, clientSecret).then(response => {
 }).catch(e => {
   console.log(e);
 });
-/*
-const token = yelp.accessToken("USLc4bO1jd_WNmHyMtyWeQ", "CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D").then(response => {
-  console.log(response.jsonBody.access_token);
-}).catch(e => {
-  console.log(e);
-});
 
-const client = yelp.client(token);
-*/
-/*
-client.search({
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
-}).then(response => {
-  console.log(response.jsonBody.businesses[0].name);
-}).catch(e => {
-  console.log(e);
-});
-*/
