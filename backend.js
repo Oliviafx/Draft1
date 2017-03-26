fafeb16b5e76eb5a710d269b655c692543d61532
Olivia Xiang <oliviafx@gmail.com>
2017-03-26 02:17:30 -0400

    did not finish editing

--- backend.js
+++ backend.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const yelp = require('yelp-fusion');
+const express = require('express');
 const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
 const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
+
+var app = express();
 
 var searchRequest = {
   location: 'london'
 };
+
+var searchTypeEnum = {
+  SEARCH: "search",
+  PHONE: "phone",
+  TRANSACTION: "transaction",
+  BUSINESS: "business",
+  REVIEWS: "reviews",
+  AUTOCOMPLETE: "autocomplete"
+}
 
 yelp.accessToken(clientId, clientSecret).then(response => {
   const client = yelp.client(response.jsonBody.access_token);
