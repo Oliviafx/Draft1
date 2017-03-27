fb773aa09c3944136546e39fe58944fd72ea7162
Olivia Xiang <oliviafx@gmail.com>
2017-03-26 20:30:15 -0400

    Delete searchHeader.js

--- searchHeader.js
+++
@@ -1,5 +0,0 @@
-$.ajaxSetup({
-    beforeSend: function(xhr) {
-        xhr.setRequestHeader('Authorization', 'Bearer rUkajLiHIMVFO3mx9jMTIY13XuM8GdVabuF1t37ew4di-l0XD1xhSZ-sKqf7ozxvYEC_WwH-Zn8JSOH73EVA_Jb2oIPDZz_IJ_D6ZE4KAb8nFc9wp6aUb-KDM77SWHYx');
-    }
-});
