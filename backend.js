3489b8e337fdf6154d553cd35bdb8f6abe89e0c5
Olivia Xiang <oliviafx@gmail.com>
2017-03-29 16:44:09 -0400

    welp

--- backend.js
+++ backend.js
@@ -5,7 +5,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
-//const fs = require('fs');
+const fs = require('fs');
+const jsdom = require('jsdom');
 
 const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
 const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
@@ -13,14 +14,15 @@ const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0
 var app = express();
 var upload = multer();
 
-app.set('port', (process.env.PORT || 8080));
-
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var htmlSource = fs.readFileSync("search.html", "utf8");
+
 
 app.post('/search', upload.array(), function(req, res){
+
 	console.log(req.body);
 
 yelp.accessToken(clientId, clientSecret).then(response => {
@@ -29,13 +31,13 @@ yelp.accessToken(clientId, clientSecret).then(response => {
    	 const firstResult = response.jsonBody.businesses[0];
    	 const prettyJson = JSON.stringify(firstResult, null, 4);
      console.log(prettyJson);
+     
 
+  });
 }).catch(e => {
   console.log(e);
 });
-
-res.json(prettyJson);
-
+	res.json(prettyJson);
 });
 
 
