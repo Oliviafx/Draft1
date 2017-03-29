d210cacd38a0da0e90d09b0e3b0f282e52b25efb
Olivia Xiang <oliviafx@gmail.com>
2017-03-29 13:45:46 -0400

    deleted some commented out code

--- backend.js
+++ backend.js
@@ -20,42 +20,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var htmlSource = fs.readFileSync("search.html", "utf8");
 
-function call_jsdom(source, callback) {
-    jsdom.env(
-        source,
-        [ 'jquery-1.7.1.min.js' ],
-        function(errors, window) {
-            process.nextTick(
-                function () {
-                    if (errors) {
-                        throw new Error("There were errors: "+errors);
-                    }
-                    callback(window);
-                }
-            );
-        }
-    );
-}
-
 
 app.post('/search', upload.array(), function(req, res){
 
 	console.log(req.body);
 
-	yelp.accessToken(clientId, clientSecret).then(response => {
-  const client = yelp.client(response.jsonBody.access_token);
-  client.search(req.body).then(response => {
-    const firstResult = response.jsonBody.businesses[0];
-    const prettyJson = JSON.stringify(firstResult, null, 4);
-    console.log(prettyJson);
-    //document.getElementById("textresults").innerHTML = prettyJson;
-    //res.writeHead(prettyJson, {'Content-Type': 'text/html'});/
+yelp.accessToken(clientId, clientSecret).then(response => {
+  	const client = yelp.client(response.jsonBody.access_token);
+ 	client.search(req.body).then(response => {
+   	 const firstResult = response.jsonBody.businesses[0];
+   	 const prettyJson = JSON.stringify(firstResult, null, 4);
+     console.log(prettyJson);
+     res.json(prettyJson);
 
-    call_jsdom(htmlSource, function(window){
-    	var $ = window.$;
-    	$("h2").text(prettyJson.text());
-    	//console.log(documentToSource(window.document));
-    });
+
   });
 }).catch(e => {
   console.log(e);
