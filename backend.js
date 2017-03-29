b64d2fecc1929fdf4b6ecde9203f36ebda6164db
Olivia Xiang <oliviafx@gmail.com>
2017-03-29 01:00:30 -0400

    commits for tonight

--- backend.js
+++ backend.js
@@ -5,6 +5,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+const jsdom = require('jsdom');
 
 const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
 const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
@@ -16,7 +18,25 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//var htmlSource = fs.readFileSync();
+var htmlSource = fs.readFileSync("search.html", "utf8");
+
+function call_jsdom(source, callback) {
+    jsdom.env(
+        source,
+        [ 'jquery-1.7.1.min.js' ],
+        function(errors, window) {
+            process.nextTick(
+                function () {
+                    if (errors) {
+                        throw new Error("There were errors: "+errors);
+                    }
+                    callback(window);
+                }
+            );
+        }
+    );
+}
+
 
 app.post('/search', upload.array(), function(req, res){
 
@@ -28,14 +48,21 @@ app.post('/search', upload.array(), function(req, res){
     const firstResult = response.jsonBody.businesses[0];
     const prettyJson = JSON.stringify(firstResult, null, 4);
     console.log(prettyJson);
-    document.getElementById("textresults").innerHTML = prettyJson;
-    res.writeHead(prettyJson, {'Content-Type': 'text/html'});
+    //document.getElementById("textresults").innerHTML = prettyJson;
+    //res.writeHead(prettyJson, {'Content-Type': 'text/html'});/
+
+    call_jsdom(htmlSource, function(window){
+    	var $ = window.$;
+    	$("h2").text(prettyJson.text());
+    	//console.log(documentToSource(window.document));
+    });
   });
 }).catch(e => {
   console.log(e);
 });
 
 });
+
 
 
 app.listen(8080);
