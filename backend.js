815bf9c116346e1cb15718098ec6a2c8da6c168b
Olivia Xiang <oliviafx@gmail.com>
2017-04-02 18:17:51 -0400

    trying to get back to a working version

--- backend.js
+++ backend.js
@@ -7,7 +7,6 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const jsdom = require('jsdom');
-//const localStorage = require('node-localstorage');
 var ls = require('local-storage');
 //const Heroku = require('heroku-client');
 //const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
@@ -22,46 +21,8 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//var htmlSource = fs.readFileSync("search.html", "utf8");
+
 var temp;
-
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
-  console.log("we need this");
-}
-/*
-heroku.post('/search').then(app => {
-console.log(req.body);
-
-yelp.accessToken(clientId, clientSecret).then(response => {
-  	const client = yelp.client(response.jsonBody.access_token);
- 	client.search(req.body).then(response => {
-   	 const firstResult = response.jsonBody.businesses[0];
-   	 console.log(firstResult);
-   	 const prettyJson = JSON.stringify(firstResult, null, 4);
-   	 temp = prettyJson;
-     console.log(prettyJson);
-     
-
-     var obj = JSON.parse(prettyJson);
-     /*
-     localStorage.setItem("name", obj.name);
-     localStorage.setItem("phone", obj.phone);
-     localStorage.setItem("bus_img", obj.image_url);
-  	localStorage.setItem("lat", obj.coordinates.latitude);
-  	localStorage.setItem("long", obj.coordinates.longitude);
-  	localStorage.setItem("rating", obj.rating);
-  	
-  	ls.set("name", obj.name);
-     ls.set("phone", obj.phone);
-     ls.set("bus_img", obj.image_url);
-  	ls.set("lat", obj.coordinates.latitude);
-  	ls.set("long", obj.coordinates.longitude);
-  	ls.set("rating", obj.rating);
-
-});
-*/
 
 app.post('/search', upload.array(), function(req, res){
 
