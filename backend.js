df569e618c9481cd4de233fb4b0ef716cc5d6dd9
Olivia Xiang <oliviafx@gmail.com>
2017-04-03 02:23:38 -0400

    back to localstorage copy that works

--- backend.js
+++ backend.js
@@ -7,9 +7,6 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const jsdom = require('jsdom');
-var ls = require('local-storage');
-var cookies = require('cookies');
-
 
 const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
 const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
@@ -21,7 +18,7 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+var htmlSource = fs.readFileSync("search.html", "utf8");
 var temp;
 
 app.post('/search', upload.array(), function(req, res){
@@ -38,17 +35,6 @@ yelp.accessToken(clientId, clientSecret).then(response => {
      console.log(prettyJson);
      
 
-     var obj = JSON.parse(prettyJson);
-
-     /*
-  	ls.set("name", obj.name);
-     ls.set("phone", obj.phone);
-     ls.set("bus_img", obj.image_url);
-  	ls.set("lat", obj.coordinates.latitude);
-  	ls.set("long", obj.coordinates.longitude);
-  	ls.set("rating", obj.rating);
-  	*/
-	
   });
 }).catch(e => {
   console.log(e);
@@ -56,11 +42,6 @@ yelp.accessToken(clientId, clientSecret).then(response => {
 	res.json(temp);
 });
 
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
-
-
