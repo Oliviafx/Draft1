065794b45f57a3ff854eaf41628ce1bde84179c5
Olivia Xiang <oliviafx@gmail.com>
2017-04-03 16:45:10 -0400

    changing it back to a form

--- backend.js
+++ backend.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 var temp;
 
 
-app.post('/search', upload.array(), function(req, res){
+app.post('/', upload.array(), function(req, res){
 
 	console.log(req.body);
 
