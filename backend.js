286c7963b5692668edc2e9065c3d1f5d2938fb94
Olivia Xiang <oliviafx@gmail.com>
2017-04-03 05:22:58 -0400

    trying this

--- backend.js
+++ backend.js
@@ -42,6 +42,7 @@ yelp.accessToken(clientId, clientSecret).then(response => {
 	res.json(temp);
 });
 
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
--- external.js
+++ external.js
@@ -1,1 +1,11 @@
+var results;
 
+function place_results(data){
+	results = data;
+	console.log("results: ");
+	//console.log(results);
+}
+
+function get_results(){
+	return results;
+}
