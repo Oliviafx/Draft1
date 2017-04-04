732713872a4581f27019af4c325bc5f0d827d628
Olivia Xiang <oliviafx@gmail.com>
2017-04-03 23:24:42 -0400

    New version

--- backend.js
+++ backend.js
@@ -1,53 +1,53 @@
 'use strict';
 
-const yelp = require('yelp-fusion');
+const qs = require('query-string');
 const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const jsdom = require('jsdom');
-
+var bodyParser = require('body-parser');
+const ejs = require('ejs');
+//yelp constants
+const yelp = require('yelp-fusion');
 const clientId = 'USLc4bO1jd_WNmHyMtyWeQ';
 const clientSecret = 'CgmeETa6k6WbF82MNxaRuBU0rbTuILsTRic8nBcN5brSZdyUddRWKg2AC0Zuud9D';
 
+
 var app = express();
-var upload = multer();
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname );
+app.set('view engine', 'html');
+
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-var htmlSource = fs.readFileSync("search.html", "utf8");
-var temp;
-
-app.post('/search', upload.array(), function(req, res){
-
+app.post('/', urlencodedParser, function(req, res){
 	console.log(req.body);
 
 yelp.accessToken(clientId, clientSecret).then(response => {
-  	const client = yelp.client(response.jsonBody.access_token);
- 	client.search(req.body).then(response => {
-   	 const firstResult = response.jsonBody.businesses[0];
-   	 console.log(firstResult);
-   	 const prettyJson = JSON.stringify(firstResult, null, 4);
-   	 temp = prettyJson;
-     console.log(prettyJson);
+    const client = yelp.client(response.jsonBody.access_token);
+  
+  client.search(req.body).then(response => {
+     const firstResult = response.jsonBody.businesses[0];
+     console.log(firstResult);
+     const prettyJson = JSON.stringify(firstResult, null, 4);
      
 
-  });
-}).catch(e => {
-  console.log(e);
-});
-	res.json(temp);
+     var obj = JSON.parse(prettyJson);
+     var url = obj.img_url;
+     var info = {src: url, name: obj.name, price: obj.price, rating: obj.rating, review_count: obj.review_count, distance: obj.distance}
+
+     console.log(info.name);
+
+     res.render('index', {data: info});
+	}).catch(e => {
+  		console.log(e);
+	});
+
+	});
+
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-//app.listen(8080);
-//console.log('App listening on port 8080');
-
-
-
